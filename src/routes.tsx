2d877997b856e7f609d97876a2c4a81fe58c42b9
yez1125 <yez1125lin@gmail.com>
2024-12-05 16:57:09 +0800

    Fix merge conflict

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter, type RouteObject } from "react-router-dom";
 import RootLayout from "./RootLayout";
 import Home from "./pages/Home";
-// import Page1 from "./pages/Page1";
 import MainPage from "./pages/MainPage";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
