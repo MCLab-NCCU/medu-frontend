aa0063152212c184a567a87338f38221efdbefad
yez1125 <yez1125lin@gmail.com>
2024-12-05 16:42:46 +0800

    Rebuild main page and add match function -rebuild chatroom and navbar -add match card

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,14 +1,30 @@
 import ChatRoom from "./ChatRoom";
 import FriendList from "./FriendList";
+import Match from "./Match";
 import Navbar from "./Navbar";
 
 function MainPage() {
+  /*<div className="w-3/4 bg-[#fefefe] rounded-lg m-2 border-black border-2">
+        <Match />
+      </div>
+      <div className="w-3/4 bg-[#fefefe] rounded-lg m-2 border-black border-2 relative">
+        <ChatRoom />
+      </div>
+      
+      */
+
   return (
     <div className="flex h-screen">
-      <div className="w-1/4 border-black bg-[#6cb1c9] rounded-lg  m-2">
-        <Navbar />
-        <FriendList />
+      <div className="w-1/4 border-black rounded-lg mt-4">
+        <div className="h-[15%]">
+          <Navbar />
+        </div>
+        <div className="border-black border-2"></div>
+        <div className="mt-5 h-[80%] ">
+          <FriendList />
+        </div>
       </div>
+
       <div className="w-3/4 bg-[#fefefe] rounded-lg m-2 border-black border-2 relative">
         <ChatRoom />
       </div>
