79b43ad981ca9509c0c68a8dac22aaebff33f299
yez1125 <yez1125lin@gmail.com>
2024-12-05 16:42:46 +0800

    Rebuild main page and add match function -rebuild chatroom and navbar -add match card

--- src/pages/Card.tsx
+++ src/pages/Card.tsx
@@ -1,14 +1,27 @@
-function Card() {
+import React, { forwardRef } from "react";
+
+interface CardInfoProps {
+  userID: string;
+}
+
+const Card = forwardRef<HTMLDivElement, CardInfoProps>(({ userID }, ref) => {
   return (
-    <div className="card bg-base-100 w-96 shadow-xl border-black border-2 m-auto">
-      <figure>
-        <img src="src\assets\profile_photo.png" alt="Profile_Photo" />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">姓名</h2>
-        <p>自我介紹</p>
+    <div
+      ref={ref}
+      className="grid-rows-2 bg-base-100 w-[50%] shadow-xl border-black border-2 m-auto h-full"
+    >
+      <img
+        className="block w-1/3 m-auto h-1/3"
+        src="src/assets/profile_photo.png"
+        alt="Profile_Photo"
+      />
+
+      <div className="h-2/3 flex-wrap p-5">
+        <div className="text-5xl mb-3">姓名</div>
+        <div>自我介紹</div>
       </div>
     </div>
   );
-}
+});
+
 export default Card;
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,16 @@ module.exports = {
       lg: "1024px",
       xl: "1440px",
     },
+    keyframes: {
+      spinRight: {
+        "0%, 100%": { transform: "skew-y-0" },
+        "90%": { transform: "skew-y-12" },
+      },
+      spinLeft: {
+        "0%, 100%": { transform: "skew-y-0" },
+        "90%": { transform: "-skew-y-12" },
+      },
+    },
     extend: {},
   },
   plugins: [require("daisyui")],
